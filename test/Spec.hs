c123d721fd913d5be13d9945eb5697f4e2e73d30
Sriram Sami <frizensami@gmail.com>
2019-08-01 12:03:30 -0700

    Add spec testing stub

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Tokenizer" $ do
+        it "does the thing" $ do
+            1 `shouldBe` 1
